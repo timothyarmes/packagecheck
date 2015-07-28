41f6a63db441371aa0752d23abb3d59dafef2b7a
Timothy Armes <tim@timothyarmes.com>
2015-07-28 11:29:04 +0200

    Don't stop check when package isn't found - just report it.

--- package.js
+++ package.js
@@ -22,30 +22,29 @@ var checkPackage = function(packageConstraint) {
     var result = exec('meteor show ' + packageName);
 
     if (result.status != 0) {
-      console.log('Call to `meteor show` failed.')
-      console.log(error);
-      exit(1);
+      console.log(result.stderr);
     }
+    else {
+      var lines = result.stdout.split(EOL);
+      var idx = 0;
+      var latest = null;
 
-    var lines = result.stdout.split(EOL);
-    var idx = 0;
-    var latest = null;
+      // Find the list of recent versions
+      while (idx < lines.length && lines[idx++] !== 'Recent versions:') ;
 
-    // Find the list of recent versions
-    while (idx < lines.length && lines[idx++] !== 'Recent versions:') ;
+      // Get the last line in this list
+      while (idx < lines.length && lines[idx] !== '') {
+        latest = lines[idx];
+        idx++;
+      }
 
-    // Get the last line in this list
-    while (idx < lines.length && lines[idx] !== '') {
-      latest = lines[idx];
-      idx++;
-    }
-
-    // If we have a line, get the version and test
-    if (latest !== null) {
-      latestVersion = latest.split(' ').filter(function(item) { return item != ''; })[0];
-      if (PackageVersion.lessThan(version, latestVersion)) {
-        console.log(packageName + ' version ' + latestVersion + ' is available (' + version + ' currently specified)');
-        return true;
+      // If we have a line, get the version and test
+      if (latest !== null) {
+        latestVersion = latest.split(' ').filter(function(item) { return item != ''; })[0];
+        if (PackageVersion.lessThan(version, latestVersion)) {
+          console.log(packageName + ' version ' + latestVersion + ' is available (' + version + ' currently specified)');
+          return true;
+        }
       }
     }
   }
